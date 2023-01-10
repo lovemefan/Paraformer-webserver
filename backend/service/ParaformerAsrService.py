7ab2d81c2b4e0877a1418e779a7cc2bc1e95f560
lovemefan <lovemefan@outlook.com>
2023-01-10 17:25:18 +0800

    nothing update

--- backend/service/ParaformerAsrService.py
+++ backend/service/ParaformerAsrService.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding:utf-8 -*-
-# @FileName  :WhisperAsrService.py
-# @Time      :2023/1/7 17:01
+# @FileName  :ParaformerAsrService.py
+# @Time      :2023/1/8 17:01
 # @Author    :lovemefan
 # @email     :lovemefan@outlook.com
 from typing import Union
@@ -14,12 +14,13 @@ import numpy as np
 from backend.utils.logger import logger
 from modelscope.pipelines import pipeline
 
+
 @singleton
 class ParaformerAsrService:
     def __init__(self):
-        self.model = pipeline('asr-webservice', 'damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
+        self.model = pipeline('asr-webservice',
+                              'damo/speech_paraformer-large_asr_nat-zh-cn-16k-common-vocab8404-pytorch')
 
     def transcribe(self, audio: Union[np.ndarray, File]):
         result = self.model(audio_in=audio.body)
         return result
-
